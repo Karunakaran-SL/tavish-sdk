3cba4f4c05142406c8898f5c017c4206de279e1b
Subash Chandrabose, Karunakaran <karunakaran.subashchandrabose@astrazeneca.com>
2017-03-22 10:34:56 +0530

    Added intial file

---
+++ tavish-sdk/src/main/java/com/tavish/voice/reco/sdk/CommandResponse.java
@@ -0,0 +1,8 @@
+package com.tavish.voice.reco.sdk;
+
+import lombok.Data;
+
+@Data
+public class CommandResponse {
+    private String speak;
+}
---
+++ tavish-sdk/src/main/java/com/tavish/voice/reco/sdk/TavishSdkApplication.java
@@ -0,0 +1,12 @@
+package com.tavish.voice.reco.sdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavishSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TavishSdkApplication.class, args);
+	}
+}
